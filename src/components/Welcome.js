7a6318911fa86d2bb73b14ec8e97a207fe7205ea
amitkyadav <38914912+amitkyadav@users.noreply.github.com>
2020-12-29 01:22:07 +0530

    added name and heading tag

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
-// write code for Welcome component here
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+export default function Welcome(props) {
+  const l = props.name;
+  return (
+    <div>
+      Hey !<h1>{l}</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  );
+}
